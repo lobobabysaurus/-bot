0f71fc7fa59dc94284784f0b05f61ef81ed5caa2
Phil <phil@tubbycatgames.com>
2018-10-29 19:01:53 -0400

    Updates to Responses

--- bot/main.go
+++ bot/main.go
@@ -20,46 +20,39 @@ func main() {
 	})
 
 	annoyanceResponder := NewTriggerResponder("Filbot", "", []string{
-		"хватит надоедать мне!",
-		"БЫДЛО",
-		"ДУРАК",
-		"СВОЛОЧЬ",
-		"ПОДОНОК",
-		"Сволочь",
-		"ДРЯНЬ",
+		"Shut Up Jake!",
+		"Fuck Off!",
+		"I like cats",
+		"Who's askin?",
+		"Fuck your couch",
+		"Just let me live!",
+		"I fart in your general direction!",
+		"Your mother was a hamster and your father smelt of elder berries!",
 	})
 
-	ilyaResponder := NewRandomResponder(0.05, rand.Float64, []string{
-		"When I talk English, I’m still computing.",
-		"When I hear my voice, it sounds disgusting.",
-		"Guys! A question! Is chicken meat? Or is it a bird?",
-		"In Philly, win or lose, you’re still overpaid.",
-		"Decepticons always have nice stuff." +
-			" Autobots, they rebels. They weapons aren’t as nice.",
-		"Whoa, whoa, whoa, whoa. I don’t have contract. I’m here as guest.",
-		"Siberian Husky. She’s all white. Beautiful blue eyes." +
-			" That’s basically blonde girl with blue eyes." +
-			" Your dream, man. My husky, basically, she’s a hot girl, man.",
-		"I think like, ‘And we have some problems here on the earth we worry" +
-			" about? Compared to like…nothing. Just be happy. Don’t worry be happy" +
-			" right now",
+	randomResponder := NewRandomResponder(0.05, rand.Float64, []string{
+		"It me, Filbot!",
+		"You can build a throne with bayonets, but you can't sit on it for long.",
+		"The Vegas Golden Knights are poorly constructed.",
+		"All hail Trollbot!",
+		"Yahoo Sports was made by the son of a barmaid and a donkey",
+		"Doesn't matter how good Temple's stadium will be, will never compare to Luzhniki",
+		"I wish I was a puppy",
 	})
 
 	pingResponder := NewTriggerResponder("Ping", "", []string{"понг"})
 
 	tradeResponder := NewTriggerResponder(
 		"Trade", "You give best player, I give ", []string{
-			"Bucket of Wood Chips",
-			"Concrete Crumbles",
-			"Fabergé egg",
-			"Guy Fieri",
-			"Putin's love",
-			"2020 Presidential election",
-			"Ukrainian prostitutes",
-			"Pyser's kidney",
+			"Lebron James",
+			"Meir Engel",
+			"Evgeni Malkin",
+			"Magadan Oblast",
+			"Rob Gronkowski",
+			"Pool full of Borscht."
 		})
 
 	samHandler := NewSAMHandler(
-		gmHandler, annoyanceResponder, ilyaResponder, pingResponder, tradeResponder)
+		gmHandler, annoyanceResponder, randomResponder, pingResponder, tradeResponder)
 	lambda.Start(samHandler.Handle)
 }
